995647a98b916e5af9f5527d05d1b80986818051
Bennet <bennet@metricminds.com>
2024-04-20 16:11:50 +0200

    Remove usercontrol

--- WPF_Notes/Model/Notebook.cs
+++ WPF_Notes/Model/Notebook.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace WPF_Notes.Model
 {
-    internal class Notebook
+    public class Notebook
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
